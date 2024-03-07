c61fead817323fbdc0bdf6eeea1eb19d20b65a3d
jean-chadi <jean-chadi.frere@groupe-esigelec.org>
2024-03-07 16:36:08 +0100

    Création des différentes classes

---
+++ API_Projet/API_Projet/Models/Constructor.cs
@@ -0,0 +1,10 @@
+﻿namespace API_Projet.Models
+{
+    public class Constructor
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public string country { get; set; }
+    }
+}
---
+++ API_Projet/API_Projet/Models/Sales.cs
@@ -0,0 +1,12 @@
+﻿namespace API_Projet.Models
+{
+    public class Sales
+    {
+        public int Id { get; set; }
+        public Console Console { get; set; }
+        public double UnitsSoldInMillions { get; set; }
+        public int SalesYear { get; set; }
+
+
+    }
+}
