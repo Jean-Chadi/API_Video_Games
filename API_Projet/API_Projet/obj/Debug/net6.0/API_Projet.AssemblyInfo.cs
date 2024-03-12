4d3e04194f8be396accde54cb09a3b087bbf90c2
Thomas <thomas.paris@groupe-esigelec.org>
2024-03-12 08:54:07 +0100

    Ajout de la connexion a la bdd

--- API_Projet/API_Projet/obj/Debug/net6.0/API_Projet.AssemblyInfo.cs
+++ API_Projet/API_Projet/obj/Debug/net6.0/API_Projet.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API_Projet")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+59c8f3d8d0c8ca208c563909768e8e4950f09465")]
 [assembly: System.Reflection.AssemblyProductAttribute("API_Projet")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API_Projet")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
