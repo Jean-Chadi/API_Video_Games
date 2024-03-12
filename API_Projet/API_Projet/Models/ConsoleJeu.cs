526b22250bbda8d77fcbf44743b93eaa3ead31a1
jean-chadi <jean-chadi.frere@groupe-esigelec.org>
2024-03-12 11:37:13 +0100

    Modification de la classe ConsoleJeu

--- API_Projet/API_Projet/Models/ConsoleJeu.cs
+++ API_Projet/API_Projet/Models/ConsoleJeu.cs
@@ -3,9 +3,12 @@
     public class ConsoleJeu
     {
         public int Id { get; set; }
-        public float Price { get; set; }
         public string model { get; set; }
-        public Constructor brand { get; set; }
+        public string Brand { get; set; }
+        public string CountryBrand { get; set; }
         public int ReleaseYear { get; set; }
+        public float Price { get; set; }
+        public double UnitsSoldInMillions { get; set; }
+        public int SalesYear { get; set; }
     }
 }
