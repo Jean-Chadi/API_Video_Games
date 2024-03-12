85008adf9e91877651d81a82441d299c3d748e91
Thomas <thomas.paris@groupe-esigelec.org>
2024-03-12 12:18:27 +0100

    Modification des classes pour en avoir que une

---
+++ API_Projet/Controllers/ConsoleJeusController.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using API_Projet.Data;
+using API_Projet.Models;
+
+namespace API_Projet.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ConsoleJeusController : ControllerBase
+    {
+        private readonly API_ProjetContext _context;
+
+        public ConsoleJeusController(API_ProjetContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/ConsoleJeus
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<ConsoleJeu>>> GetConsoleJeu()
+        {
+            return await _context.ConsoleJeu.ToListAsync();
+        }
+
+        // GET: api/ConsoleJeus/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<ConsoleJeu>> GetConsoleJeu(int id)
+        {
+            var consoleJeu = await _context.ConsoleJeu.FindAsync(id);
+
+            if (consoleJeu == null)
+            {
+                return NotFound();
+            }
+
+            return consoleJeu;
+        }
+
+        // PUT: api/ConsoleJeus/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutConsoleJeu(int id, ConsoleJeu consoleJeu)
+        {
+            if (id != consoleJeu.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(consoleJeu).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ConsoleJeuExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/ConsoleJeus
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<ConsoleJeu>> PostConsoleJeu(ConsoleJeu consoleJeu)
+        {
+            _context.ConsoleJeu.Add(consoleJeu);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetConsoleJeu", new { id = consoleJeu.Id }, consoleJeu);
+        }
+
+        // DELETE: api/ConsoleJeus/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteConsoleJeu(int id)
+        {
+            var consoleJeu = await _context.ConsoleJeu.FindAsync(id);
+            if (consoleJeu == null)
+            {
+                return NotFound();
+            }
+
+            _context.ConsoleJeu.Remove(consoleJeu);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool ConsoleJeuExists(int id)
+        {
+            return _context.ConsoleJeu.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ API_Projet/Models/ConsoleJeu.cs
@@ -0,0 +1,14 @@
+﻿namespace API_Projet.Models
+{
+    public class ConsoleJeu
+    {
+        public int Id { get; set; }
+        public string Model { get; set; }
+        public string Brand { get; set; }
+        public string CountryBrand { get; set; }
+        public int ReleaseYear { get; set; }
+        public float Price { get; set; }
+        public double UnitsSoldInMillions { get; set; }
+        public int SalesYear { get; set; }
+    }
+}
