1d52157b40d6f5dd2c0fd7ed1b122d1af8360f52
Thomas <thomas.paris@groupe-esigelec.org>
2024-03-12 09:06:08 +0100

    Création API fonctionnelle

--- API_Projet/API_Projet/Program.cs
+++ API_Projet/API_Projet/Program.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using API_Projet.Data;
+using API_Projet.Models;
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<API_ProjetContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("API_ProjetContext") ?? throw new InvalidOperationException("Connection string 'API_ProjetContext' not found.")));
@@ -13,6 +14,12 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    SeedData.Initialize(services);
+}
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
--- API_Projet/API_Projet/obj/Debug/net6.0/API_Projet.AssemblyInfo.cs
+++ API_Projet/API_Projet/obj/Debug/net6.0/API_Projet.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API_Projet")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+59c8f3d8d0c8ca208c563909768e8e4950f09465")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4d3e04194f8be396accde54cb09a3b087bbf90c2")]
 [assembly: System.Reflection.AssemblyProductAttribute("API_Projet")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API_Projet")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
